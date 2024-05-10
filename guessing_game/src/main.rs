0eb8686ed95ce386f1f6e5283a2abc5574383c59
desksavier <joaquin.xavier@gmail.com>
2024-05-10 16:28:55 +0800

    Completed guessing game!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,30 +3,53 @@
 // This set is called a `prelude`.
 // If a type is not in the prelude, it has to be brought in via the 'use' statement.
 
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io; // Input/output library is brought into scope.
 
 // `fn` declares a new function
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    // 'let' creates a variable to store user input
-    // Variables are immutable by default in Rust.
-    // Every value is essentially a const/constexpr
-    // `mut` makes a variable mutable`
-    let mut guess = String::new(); // This variable binds to the return value of String::new()
-    // :: means that new is an associative function of String
-    // Create a mutable value that is bound to a new instance of a String.
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        // 'let' creates a variable to store user input
+        // Variables are immutable by default in Rust.
+        // Every value is essentially a const/constexpr
+        // `mut` makes a variable mutable`
+
+        let mut guess = String::new(); // This variable binds to the return value of String::new()
+
+        // :: means that new is an associative function of String
+        // Create a mutable value that is bound to a new instance of a String.
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
 
 // Processing a Guess
-// 1. Take input / 
+// 1. Take input /
 
 // Binary crate = executable
 // Library crate = dependency
